4dd3843a21644c993a6704e243487d597c196f49
William Gabriel <80060962+WillAcris@users.noreply.github.com>
2025-11-18 14:40:09 -0400

    33444

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { FavoritesProvider } from './contexts/FavoritesContext';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
